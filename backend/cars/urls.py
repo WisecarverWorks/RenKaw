11650785b4c2a3801f7b6efdab37e7ce46bce1b1
Kyle Wisecarver <102608427+WisecarverWorks@users.noreply.github.com>
2022-06-03 00:45:41 -0500

    chore: created url patterns for ren app.

--- backend/cars/urls.py
+++ backend/cars/urls.py
@@ -4,6 +4,6 @@ from cars import views
 # <<<<<<<<<<<<<<<<< EXAMPLE FOR STARTER CODE USE <<<<<<<<<<<<<<<<<
 
 urlpatterns = [
-    path('', views.user_cars),
+    path('', views.user_cars), ## Notice these are routing to functions not classes. 
     path('all/', views.get_all_cars),
 ]
---
+++ backend/ren/urls.py
@@ -0,0 +1,17 @@
+# We must import path and include from django
+
+#from django.urls import path, include
+
+# We want to import the views file from our Ren app
+
+#from ren import views
+
+#<<<<<<<<<<<<<<<<<<<<< Below this line we will provide a variable set equal to a list of url patterns. 
+
+# Our patterns variable will be named ren_urlpatters, here we will route functions 
+ren_urlpatterns = [
+    # Here we will assign our first path, an open string  
+    #path('', views.user_ren),
+    # Here our second path
+    #path('all/', viewa.user_ren),
+]
