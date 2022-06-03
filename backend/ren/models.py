0c4462a143d3451a6dc472878ec3cd9654141128
Kyle Wisecarver <102608427+WisecarverWorks@users.noreply.github.com>
2022-06-03 01:02:20 -0500

    chore: Created register for art.

--- backend/ren/models.py
+++ backend/ren/models.py
@@ -10,7 +10,12 @@ from authentication.models import User
 class Art(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE) 
     # This is a one to many relationship, A work of art will have one user, but one user can have many works of art. 
-
+    type = models.CharField(max_length=30)
+    year = models.IntegerField()
+    name = models.CharField(max_length=100)
+    price = models.IntegerField()
     # We will need the different types of art, the year it was made, the name and the price of the item. 
 
-    
+
+# Do we need a class for members? 
+
