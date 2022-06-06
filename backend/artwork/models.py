20ffe851a4436c171ba46f44c290cc956d10c653
Kyle Wisecarver <102608427+WisecarverWorks@users.noreply.github.com>
2022-06-06 16:25:34 -0500

    docs: models created for Member app / Artwork app they each have a user foriegnkey and either member or artwork id.

--- backend/artwork/models.py
+++ backend/artwork/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from authentication.models import User
 # Create your models here.
+
+# <<<<<< Model for Members >>>>>>
+
+class Artwork(models.Model):
+    # user will handle our foriegn key for members 
+    # customerid will hold a key
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    artworkId = models.IntegerField()
+    artworkTitle = models.CharField(max_length=25)
+    artworkStyle = models.CharField(max_length=25)
+    artworkAbout = models.CharField(max_length=100)
+    artworkPrice = models.IntegerField()
+    
+# We will add a line of code in settings file to make sure integerfields only have full number values 
--- backend/member/models.py
+++ backend/member/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from authentication.models import User
 # Create your models here.
+
+# <<<<<< Model for Members >>>>>>
+
+class Member(models.Model):
+    # user will handle our foriegn key for members 
+    # customerid will hold a key
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    memberId = models.IntegerField()
+    firstName = models.CharField(max_length=25)
+    lastName = models.CharField(max_length=25)
+    city = models.CharField(max_length=25)
+    street = models.IntegerField()
+    zipcode = models.IntegerField()
+    state = models.CharField(max_length=25)
+
+# We will add a line of code in settings file to make sure integerfields only have full number values 
