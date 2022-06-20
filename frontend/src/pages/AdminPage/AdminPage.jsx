061c7dd880ebb85adef59f9d76a2232708d24806
Kyle Wisecarver <102608427+WisecarverWorks@users.noreply.github.com>
2022-06-20 18:42:29 -0500

    Assessment is finished with a Rich text editor involved.

--- frontend/src/pages/AdminPage/AdminPage.jsx
+++ frontend/src/pages/AdminPage/AdminPage.jsx
@@ -14,21 +14,20 @@ const AdminPage = () => {
                         <h1>ADMIN</h1>
                         <body>
                             <form >
-                          <label for='Assessment-Title'>Assessment Title</label>
-                          <input type="text" id="Assessment-Title" placeholder="Assessment" />
-                          <label for="Assessment-Body">Assessment Body</label>
-                          <MyEditor type="text" id="Assessment-Body" />
-                          <PreviewModal />
-                          <label for="Date-Time">Date-Time</label>
-                          <input type="date" id="Date-Time"/>
-                          <select  name="Asssessment-File">
-                            <option value='.pdf'>PDF</option>
-                            <option value='.png'>PNG</option>
-                            <option value='.txt'>TXT</option>
-                          </select>
+                                <label for='Assessment-Title'>Assessment Title</label>
+                                <input type="text" id="Assessment-Title" placeholder="Assessment" />
+                                <MyEditor type="text" id="Assessment-Body" />
+                                <PreviewModal />
+                                <label for="Date-Time">Date-Time</label>
+                                <input type="date" id="Date-Time"/>
+                                <select  name="Asssessment-File">
+                                <option value='.pdf'>PDF</option>
+                                <option value='.png'>PNG</option>
+                                <option value='.txt'>TXT</option>
+                            </select>
                           <input type="submit" value="Submit" onClick/>
-                        </form>
-                    </body>
+                            </form>
+                        </body>
                     </div>      
                 </div>
             </div>
