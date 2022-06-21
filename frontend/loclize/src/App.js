74ba8719dd235f44743c6ec76ae068779f5cd24a
Kyle Wisecarver <102608427+WisecarverWorks@users.noreply.github.com>
2022-06-20 22:17:17 -0500

    docs: Completed the integration of i18next for localization.

--- frontend/loclize/src/App.js
+++ frontend/loclize/src/App.js
@@ -1,14 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 import { useTranslation, Trans } from 'react-i18next';
+import { useState } from 'react';
+
+const lngs = {
+  en: { nativeName: 'English' },
+  de: { nativeName: 'Deutsch' }
+};
 
 function App() {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
+  const [count, setCounter] = useState(0);
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <div>
+          {Object.keys(lngs).map((lng) => (
+            <button key={lng} style={{ fontWeight: i18n.resolvedLanguage === lng ? 'bold' : 'normal' }} type="submit" onClick={() => {
+              i18n.changeLanguage(lng);
+              setCounter(count + 1);
+            }}>
+              {lngs[lng].nativeName}
+            </button>
+          ))}
+        </div>
         <p>
           <Trans i18nKey="desctiption.part1">
             Edit <code>src/App.js</code> and save to reload.
